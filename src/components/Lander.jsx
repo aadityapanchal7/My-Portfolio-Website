3c1fcddf25f671addc591b3e14560d90a4367840
wilder09 <aadityapanchal289@gmail.com>
2024-06-14 17:30:46 -0500

    Revert "idk"
    This reverts commit ffca43c3e50f72640b06075dfadcd7692fc5ee02.

--- src/components/Lander.jsx
+++ src/components/Lander.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Emoji from './Emoji';
-import ReactTyped from 'react-typed';  
+import { ReactTyped } from 'react-typed';
 import { FaArrowRight } from 'react-icons/fa';
 import { Links } from '../utils/Links';
 
@@ -8,11 +8,9 @@ function Lander() {
   return (
     <div className='flex flex-col h-screen leading-relaxed text-black md:box-border md:w-1/2 items-start md:pl-20 pl-16 mt-5 md:mt-0 md:fixed md:justify-center'>
       <Emoji />
-
       <h1 className='text-4xl md:text-5xl font-thin'>Hey, I am</h1>
       <h1 className='text-4xl font-bold text-blue-500 md:text-6xl text-balance'>Aaditya Panchal</h1>
       <div className='flex flex-col flex-wrap justify-center pt-3 md:flex-row md:px-0 md:justify-start'>
-
         <p className='text-balance text-sm'>
           a self-taught programmer with an <br className='md:hidden block' /> interest in 
           <ReactTyped 
@@ -28,7 +26,7 @@ function Lander() {
             typeSpeed={70}
             backSpeed={50}
             loop
-            className='pl-1'
+            className='pl-1 '
           />
         </p>
       </div>
@@ -37,17 +35,17 @@ function Lander() {
         <ul>
           <li className='transition-all hover:translate-x-5 hover:text-blue-400'>
             <a href='#About'>
-              <span className='flex items-center'><FaArrowRight className='mr-1' /> About</span>
+              <span className='flex items-center'><FaArrowRight className='mr-1'/> About</span>
             </a>
           </li>
           <li className='transition-all hover:translate-x-5 hover:text-blue-400'>
             <a href='#Experience'>
-              <span className='flex items-center'><FaArrowRight className='mr-1' /> Experience</span>
+              <span className='flex items-center'><FaArrowRight className='mr-1'/> Experience</span>
             </a>
           </li>
           <li className='transition-all hover:translate-x-5 hover:text-blue-400'>
             <a href='#Projects'>
-              <span className='flex items-center'><FaArrowRight className='mr-1' /> Projects</span>
+              <span className='flex items-center'><FaArrowRight className='mr-1'/> Projects</span>
             </a>
           </li>
         </ul>
