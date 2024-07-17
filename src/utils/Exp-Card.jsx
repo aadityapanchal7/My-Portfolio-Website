510daf7ccb01d5b8a23bff96c9d002eac92a0170
wilder09 <aadityapanchal289@gmail.com>
2024-07-17 15:05:38 -0500

    updated name

--- src/utils/Exp-Card.jsx
+++ src/utils/Exp-Card.jsx
@@ -9,13 +9,13 @@ export const Experience = [
     Description: 'Stat-Oracle is a soccer startup where users upload game clips to be processed with AI to gain stats and insights. Developed and optimized the user interface for a machine learning database, enhancing usability and user experience. Implemented a waitlist feature using the Waitlist API, improving customer acquisition and retention.',
   },
   {
-    image: './assets/Neuro_Logo.png',
+    image: './assets/CortexPD.webp',
     width: '80',
     alt: 'Logo',
-    Title: 'Neuro-Catalyst',
+    Title: 'CortexPD',
     Role: 'Frontend Dev', 
     Date: '/ June 2024 - Present',
-    Description: 'Neuro-Catalyst is a nonprofit organization dedicated to improving the lives of people diagnosed with Parkinson\'s disorder. Designed and implemented a comprehensive website, focusing on user interface and overall design to improve user engagement.',
+    Description: 'CortexPD is a nonprofit organization dedicated to improving the lives of people diagnosed with Parkinson\'s disorder. Designed and implemented a comprehensive website, focusing on user interface and overall design to improve user engagement.',
   },
   {
     image: './assets/AP-Visuals.png',
